34a094224c74fc9f5fd28b206a6de552c6773197
rob102910 <47224682+rob102910@users.noreply.github.com>
2019-02-21 15:07:40 -0500

    Update collegescraper.js

--- collegescraper.js
+++ collegescraper.js
@@ -1,1 +1,17 @@
+//For majors,minors,professional programs, and academic offereings//
+var s = document.createElement("script");
+s.src = "http://d3js.org/d3.v5.js"
+var body = document.getElementsByTagName("body")[0];
+body.after(s);
 
+d3.selectAll(".fusion-text a").nodes().forEach(function(e){console.log(e.innerText)});
+
+//For classes offered a Centre//
+var s = document.createElement("script");
+s.src = "http://d3js.org/d3.v5.js"
+var body = document.getElementsByTagName("body")[0];
+body.after(s);
+
+d3.selectAll("#main li").nodes().forEach(function(e){console.log(e.innerText)});
+
+//
